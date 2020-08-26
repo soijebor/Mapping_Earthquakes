dc6c9ab24d5229935766778d251c4737fdec3c61
Sonia Ijebor <sonia_ij@macbook-pro.home>
2020-08-26 16:18:56 -0500

    adding file

--- Earthquakes_past7days/Simple_Map/static/js/logicStep2.js
+++ Earthquakes_past7days/Simple_Map/static/js/logicStep2.js
@@ -56,10 +56,6 @@ function getRadius(magnitude) {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/soijebor/Mapping_Earthquakes/master/torontoNeighborhoods.json";
-
-
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
 // Creating a GeoJSON layer with the retrieved data.
